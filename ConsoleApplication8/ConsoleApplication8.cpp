ff82765888cbdc11d1bf08e623d1d9b30d4a2ed1
Sh0uj0Rei <dmitry.svitkov1@gmail.com>
2024-10-29 14:34:25 +0700

    First commit

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Node
@@ -6,11 +7,100 @@ struct Node
     int numbr;
     Node* next;
 };
-
-struct M
+struct NodeM
 {
-
+    string data;
+    NodeM* next;
+    NodeM* previous;
 };
+
+struct NodeL
+{
+    int data;
+    NodeL* next;
+    NodeL* previous;
+};
+
+void Add1M(NodeM*& head, string data)//в конец
+{
+    NodeM* newM = new NodeM;
+    newM->data = data;
+    newM->next = nullptr;
+    newM->previous = nullptr;
+
+    if (!head) {
+        head = newM;
+    }
+    else {
+        NodeM* temp = head;
+        while (temp->next) {
+            temp = temp->next;
+        }
+        temp->next = newM;
+        newM->previous = temp;
+    }
+}
+
+void AddBigMac(NodeM*& head,int index, string data)// по индексу(типо индекс бигмака)
+{
+    
+}
+
+void showM(NodeM* head)
+{
+    NodeM* temp = head;
+    while (temp)
+    {
+        cout << temp->data << endl;
+        temp=temp->next;
+    }
+}
+void sizeM(NodeM* head)
+{
+    int size = 0;
+    NodeM* temp = head;
+    while (temp)
+    {
+        size++;
+        temp = temp->next;
+    }
+    cout << size;
+}
+void swapM(NodeM* head, int index,string data)
+{
+    NodeM* temp = head;
+    bool valid = 0;
+    if (index < 0)
+    {
+        valid = 0;
+    }
+    for (int i = 0; i < index; i++)
+    {
+        if (!temp)
+        {
+            valid = 0;
+        }
+        else
+        {
+            valid = 1;
+        }
+    }
+    if (valid==0)
+    {
+        cout << "invalid index";
+    }
+    else
+    {
+        for (int i = 0; i < index; i++)
+        {
+            temp = temp->next;
+        }
+        temp->data = data;
+    }
+}
+
+
+
 struct L
 {
 
@@ -22,12 +112,12 @@ struct Q
     Node* first;
     Node* last;
 };
-void Add(Q& queue, int number)
+void AddQ(Q& queue, int number)
 {
     Node* newnode = new Node;
     newnode->numbr = number;
     newnode->next = nullptr;
-    if (!queue.first) //вот тут кароч мы в войд передаем ссылку на значение и его прям меняем, если пусто
+    if (queue.first==nullptr) //вот тут кароч мы в войд передаем ссылку на значение и его прям меняем, если пусто
     {
         queue.first = newnode;
         queue.last = newnode;
@@ -39,12 +129,75 @@ void Add(Q& queue, int number)
     }
 
 }
+void DelQ(Q& queue)//ес чо бахни отчистку памяти потомыыыыы
+{
+    if (queue.first==nullptr)
+    {
+        cout << "empty queue"; //если очередь пустая, то ошибка
+    }
+
+    Node* clr = queue.first;
+    //int removed = clr->numbr;
+
+    queue.first = queue.first->next;//убирается элемент
+
+    if (queue.first==nullptr)
+    {
+        queue.last = nullptr;
+    }
+    delete clr;
+}
+void ShowQ(const Q& queue)
+{
+    Node* showtime = queue.first;
+    while (showtime)
+    {
+        cout << showtime->numbr << endl;
+    }
+}
+
 
 
 struct S
 {
-
+    Node* head = nullptr;
 };
+void AddS(S* stack, const int& data)
+{
+    Node* new_node = new Node{ data, nullptr };
+    new_node->next = stack->head;
+    stack->head = new_node;
+}
+void DelS(S* stack)
+{
+    if (stack->head == nullptr) 
+    {
+        cout << "Stack empty" << endl;
+    }
+
+    Node* new_head = stack->head->next;
+    delete stack->head;
+    stack->head = new_head;
+}
+void ShowS(S* stack)
+{
+    if (stack->head == nullptr)
+    {
+        cout << "Stack empty" << endl;
+    }
+
+    Node* showtime = stack->head;
+
+
+    
+    while (showtime != nullptr) {
+        cout << showtime->numbr << " ";
+        showtime = showtime->next;
+    }
+    cout << endl;
+}
+
+
 struct H
 {
 
